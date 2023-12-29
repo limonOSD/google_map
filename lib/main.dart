10b6ddd9403123447658f271d27a8f6315f67ced
limon <limonpaulargo47@gmail.com>
2023-12-29 23:25:43 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_map/location_screen.dart';
+
+void main() {
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LocationScreen(),
+    );
+  }
+}
